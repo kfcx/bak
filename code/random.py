e6337b0d25c37d2398a87f71c42e1a21550c2e40
Naihe <64341459+kfcx@users.noreply.github.com>
2025-03-13 11:21:10 +0800

    Add files via upload

---
+++ code/random.py
@@ -0,0 +1,79 @@
+import random
+
+
+def rand_delay_before(last_rand=0):
+    rndmean = 0.71
+    rnsigma = 0.123
+    randmin = 0.3252412
+    randmax = 1.0254214
+    mindiffbefore = 0.09
+    maxdiffbefore = 0.33
+    randfnc = random.gauss
+
+    rand = randfnc(rndmean, rnsigma)
+    # Noise
+    if random.randint(1, 10) == 1:
+        if random.randint(1, 2) == 1:
+            rand += random.uniform(mindiffbefore, maxdiffbefore)
+        else:
+            rand -= random.uniform(mindiffbefore, maxdiffbefore)
+    # 确保范围内
+    not_inside_bounds = randmin > rand or rand > randmax
+    # 确保不小于最小差异
+    not_mindiffbefore = abs(rand - last_rand) < mindiffbefore
+    # 确保不超过最大差异
+    not_maxdiffbefore = abs(rand - last_rand) > maxdiffbefore
+
+    while not_inside_bounds or not_mindiffbefore or not_maxdiffbefore:
+        rand = randfnc(rndmean, rnsigma)
+        if random.randint(1, 10) == 1:
+            if random.randint(1, 2) == 1:
+                rand += random.uniform(mindiffbefore, maxdiffbefore)
+            else:
+                rand -= random.uniform(mindiffbefore, maxdiffbefore)
+        not_inside_bounds = randmin > rand or rand > randmax
+        not_mindiffbefore = abs(rand - last_rand) < mindiffbefore
+        not_maxdiffbefore = abs(rand - last_rand) > maxdiffbefore
+
+    return rand
+
+
+def rand_delay_between(last_rand=0):
+    rndmean = 0.118
+    rnsigma = 0.02
+    randmin = 0.0728231
+    randmax = 0.1856878
+    mindiffbefore = 0.025
+    maxdiffbefore = 0.080
+
+    randfnc = random.gauss
+
+    rand = randfnc(rndmean, rnsigma)
+    if random.randint(1, 5) == 1:
+        if rand < rndmean:
+            rand += random.uniform(mindiffbefore, maxdiffbefore)
+        elif rand > rndmean and random.randint(1, 3) == 1:
+            rand -= random.uniform(mindiffbefore, maxdiffbefore)
+    # 确保范围内
+    not_inside_bounds = randmin > rand or rand > randmax
+    # 确保不小于最小差异
+    not_mindiffbefore = abs(rand - last_rand) < mindiffbefore
+    # 确保不超过最大差异
+    not_maxdiffbefore = abs(rand - last_rand) > maxdiffbefore
+
+    while not_inside_bounds or not_mindiffbefore or not_maxdiffbefore:
+        rand = randfnc(rndmean, rnsigma)
+        if random.randint(1, 5) == 1:
+            if rand < rndmean:
+                rand += random.uniform(mindiffbefore, maxdiffbefore)
+            elif rand > rndmean and random.randint(1, 3) == 1:
+                rand -= random.uniform(mindiffbefore, maxdiffbefore)
+        not_inside_bounds = randmin > rand or rand > randmax
+        not_mindiffbefore = abs(rand - last_rand) < mindiffbefore
+        not_maxdiffbefore = abs(rand - last_rand) > maxdiffbefore
+
+    return rand
+
+
+a = rand_delay_before()
+b = rand_delay_between()
